fb4572e859df655c4ca2a666f851f3fea4362f87
Zach Byerly <zbyerly@phys.lsu.edu>
2012-07-11 16:56:12 -0500

    finished adding rotation support, not yet tested

--- velocity.f90
+++ velocity.f90
@@ -4,16 +4,27 @@ subroutine velocity(nx,ny,mom_x,mom_y,rho,v_x,v_y,omega_grid,x,y)
   double precision :: rho(nx,ny)
   double precision :: mom_x(nx,ny),mom_y(nx,ny)
   double precision :: v_x(nx,ny),v_y(nx,ny)
-  double precision :: x(nx,ny),y(nx,ny)
+  double precision :: x(nx,ny),y(nx,ny),r(nx,ny),theta(nx,ny)
 
 
-  double precision :: omega_grid
+  double precision :: omega_grid, v_grid_x(nx,ny), v_grid_y(nx,ny)
+
+  call getrtheta(nx,ny,x,y,r,theta)
+
 
   !$OMP PARALLEL DO PRIVATE(i,j)
   do j=1,ny
      do i=1,nx
-        v_x(i,j) = mom_x(i,j)/rho(i,j)
-        v_y(i,j) = mom_y(i,j)/rho(i,j)
+!        v_x(i,j) = mom_x(i,j)/rho(i,j)
+!        v_y(i,j) = mom_y(i,j)/rho(i,j)
+
+        v_grid_x(i,j) = omega_grid*y(i,j)
+        v_grid_y(i,j) = -omega_grid*x(i,j)
+
+        v_x(i,j) = mom_x(i,j)/rho(i,j)-v_grid_x(i,j)
+        v_y(i,j) = mom_y(i,j)/rho(i,j)-v_grid_y(i,j)
+
+
      end do
   end do
   !$OMP END PARALLEL DO
