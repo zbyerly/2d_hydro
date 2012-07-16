a58132985b313e186e92cc9ce743f765331bcc69
Zach Byerly <zbyerly@phys.lsu.edu>
2012-07-16 10:25:40 -0500

    changing signs on grid velocity

--- velocity.f90
+++ velocity.f90
@@ -24,8 +24,8 @@ subroutine velocity(nx,ny,mom_x,mom_y,rho,v_x,v_y,omega_grid,x,y)
 !        v_x(i,j) = mom_x(i,j)/rho(i,j)
 !        v_y(i,j) = mom_y(i,j)/rho(i,j)
 
-        v_grid_x(i,j) = omega_grid*y(i,j)
-        v_grid_y(i,j) = -omega_grid*x(i,j)
+        v_grid_x(i,j) = -omega_grid*y(i,j)
+        v_grid_y(i,j) = omega_grid*x(i,j)
 
         v_x(i,j) = mom_x(i,j)/rho(i,j)-v_grid_x(i,j)
         v_y(i,j) = mom_y(i,j)/rho(i,j)-v_grid_y(i,j)
